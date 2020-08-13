e8052325f4a0d5352cde819c0305f4d2a98b4344
Cristian Orozco <cristianorozco1984@gmail.com>
2020-08-13 17:37:13 -0500

    user wdata ith Context AÃPI and reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import SpotifyWebApi from 'spotify-web-api-js'
 
 import Login from './components/Login/Login'
+import Player from './components/Player/Player'
 import { getTokenFromUrl } from './config/spotify'
+import { useDataLayerValue } from './components/Reducer/DataLayer'
+import './App.css'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+	const [token, setToken] = useState(null)
+	const [{ user }, dispatch] = useDataLayerValue()
+
 	useEffect(() => {
 		const hash = getTokenFromUrl()
 		window.location.hash = ''
 
 		const _token = hash.access_token
 
-		console.log('TOKEN', _token)
+		if (_token) {
+			setToken(_token)
+			spotify.setAccessToken(_token)
+
+			spotify.getMe().then((user) => {
+				console.log('person ', user)
+
+				dispatch({
+					type: 'SET_USER',
+					user,
+				})
+			})
+		}
+		console.log('TOKEN', token)
 	}, [])
 
-	return (
-		<div className='App'>
-			<Login />
-		</div>
-	)
+	console.log('user', user)
+
+	return <div className='app'>{token ? <Player /> : <Login />}</div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+
+import App from './App'
+import { DataLayer } from './components/Reducer/DataLayer'
+import reducer, { initialState } from './components/Reducer/reducer'
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<DataLayer initialState={initialState} reducer={reducer}>
+			<App />
+		</DataLayer>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
